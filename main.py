ec96f3f3f0a924eed3340ace1851f194b20e58f7
Antonina Vasylieva <an.vasylieva@gmail.com>
2023-04-27 17:36:56 +0300

    added main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((500, 500), pygame.RESIZABLE)
+
+run = True
+while run:
+    pygame.draw.rect(screen, 'red', [200, 200, 100, 100])
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.flip()
+pygame.quit()
